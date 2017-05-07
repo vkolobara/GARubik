1e813d8b71278c60d73369b68da33a8860e24a30
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-05-07 21:09:32 +0200

    Napravljena prezentacija, mjerenja, demonstracija, jos ispravit seminar sutra

--- src/main/scala/MainJGAP.scala
+++ src/main/scala/MainJGAP.scala
@@ -4,108 +4,183 @@ import scala.util.control.Breaks._
 import org.jgap._
 import org.jgap.impl._
 import main.java.GreedyMutation
-import main.scala.MainJGAP.{conf, rub}
 import org.jgap.event.EventManager
+import java.io.PrintWriter
+import java.io.File
+import java.io.BufferedWriter
+import java.io.FileWriter
 
 object MainJGAP extends App {
 
-  if (args.length != 1) {
-    throw new IllegalArgumentException("Command line parameter expected: number of scrambles")
+  if (args.length != 7) {
+    throw new IllegalArgumentException("Command line parameter expected: number of scrambles, population size, iteration number, crossover, greedy, mutation, repeats")
   }
 
-  var rub = new RubiksCube
+  val file = "results/cubies_fitness/scrambles%d_pop%d_cross%.2f_greedy%d_mutation%d.txt".format(args(0).toInt, args(1).toInt, args(3).toDouble, args(4).toInt, args(5).toInt)
 
-  val scramble = rub.scramble(args(0).toInt)
-  println(rub)
-  println(rub.isSolved)
+  //for (i <- 1 to args(6).toInt) {
 
-  println(rub.copy)
+    var rub = new RubiksCube
 
-  var conf: Configuration = new GAConfiguration(rub)
+    val scramble = rub.scramble(args(0).toInt)
+    println(rub)
+    println(rub.isSolved)
 
-  val sampleGenes: List[Gene] = (for (i <- 1 to 40) yield new IntegerGene(conf, 0, 17)).toList
+    println(rub.copy)
 
-  val sampleChromosome = new Chromosome(conf, sampleGenes.toArray)
-  conf.setSampleChromosome(sampleChromosome)
-  conf.setPopulationSize(50)
+    var conf: Configuration = new GAConfiguration(rub, args(3).toDouble, args(4).toInt, args(5).toInt)
 
-  var population: Genotype = Genotype.randomInitialGenotype(conf)
+    val sampleGenes: List[Gene] = (for (i <- 1 to 40) yield new IntegerGene(conf, 0, 17)).toList
 
-breakable(for (i <- 1 to 1000) {
-    population.evolve();
-    val fittest: IChromosome = population.getFittestChromosome
+    val sampleChromosome = new Chromosome(conf, sampleGenes.toArray)
+    conf.setSampleChromosome(sampleChromosome)
+    conf.setPopulationSize(args(1).toInt)
 
-    println("Gen " + i + ": current best = " + fittest.getFitnessValue);
-    if (fittest.getFitnessValue >= 48.0) break
-  }
-)
+    var population: Genotype = Genotype.randomInitialGenotype(conf)
 
-  println(population.getFittestChromosome)
+    val f = new File(file)
+    f.getParentFile.mkdirs
 
-  println(rub)
+    //val pw = new PrintWriter(new BufferedWriter(new FileWriter(f, true)))
 
-  printCubeWithSolution(rub, population.getFittestChromosome)
+    breakable(for (i <- 1 to args(2).toInt) {
+      population.evolve();
+      val fittest: IChromosome = population.getFittestChromosome
 
-  val best = population.getFittestChromosome
-  
-  def printCubeWithSolution(cube: RubiksCube, chromosome: IChromosome) = {
-    val cubeCopy = cube.copy
-    println(chromosome.getApplicationData)
-    for (i <- 0 until chromosome.getApplicationData.asInstanceOf[Int]) {
-      cubeCopy.rotateSide(chromosome.getGene(i).getAllele.asInstanceOf[Int])
+      println("Gen " + i + ": current best = " + fittest.getFitnessValue);
+
+      //pw.write(i + "\t" + fittest.getFitnessValue + "\n")
+      if (fittest.getFitnessValue >= 1.0) break
+    })
+
+    //pw.close
+
+    println(population.getFittestChromosome)
+
+    println(rub)
+    val best = population.getFittestChromosome
+
+    printCubeWithSolution(rub, best)
+
+
+    def printCubeWithSolution(cube: RubiksCube, chromosome: IChromosome) = {
+      val cubeCopy = cube.copy
+      println(chromosome.getApplicationData)
+      for (i <- 0 until chromosome.getApplicationData.asInstanceOf[Int]) {
+        cubeCopy.rotateSide(chromosome.getGene(i).getAllele.asInstanceOf[Int])
+      }
+      println(cubeCopy)
     }
-    println(cubeCopy)
-  }
-    
-  println("THE SCRAMBLE SOLVED: ")
-  println(RubiksCube.decode(scramble, scramble.length))
-  
-  println("STEPS TO REPRODUCE: ")
-  println(RubiksCube.decode((best.getGenes map { x => x.getAllele.asInstanceOf[Int] }).toVector, best.getApplicationData.asInstanceOf[Int]))
 
+    println("THE SCRAMBLE SOLVED: ")
+    println(RubiksCube.decode(scramble, scramble.length))
+
+    println("STEPS TO REPRODUCE: ")
+    println(RubiksCube.decode((best.getGenes map { x => x.getAllele.asInstanceOf[Int] }).toVector, best.getApplicationData.asInstanceOf[Int]))
+  //}
 }
 
-
-class GAConfiguration(rubiksCube: RubiksCube) extends Configuration {
-
+class GAConfiguration(rubiksCube: RubiksCube, crossover: Double, greedy: Int, mut: Int) extends Configuration {
+  Configuration.reset
+  
   this.setBreeder(new GABreeder)
   this.setRandomGenerator(new StockRandomGenerator)
   this.setEventManager(new EventManager)
   this.setMinimumPopSizePercent(0)
-  this.setSelectFromPrevGen(0.6)
+  this.setSelectFromPrevGen(0.3)
   this.setKeepPopulationSizeConstant(true)
   this.setFitnessEvaluator(new DefaultFitnessEvaluator)
   this.setChromosomePool(new ChromosomePool)
   this.setPreservFittestIndividual(true)
-  this.setKeepPopulationSizeConstant(true)
   this.setFitnessFunction(new SideCompleteFitness(rubiksCube))
-  this.addGeneticOperator(new CrossoverOperator(this, 0.5))
-  this.addGeneticOperator(new GreedyMutation(this, 1))
-  this.addGeneticOperator(new MutationOperator(this, 10))
-  this.addNaturalSelector(new TournamentSelector(this, 5, 0.7), true)
-
+  this.addGeneticOperator(new CrossoverOperator(this, crossover))
+  this.addGeneticOperator(new GreedyMutation(this, greedy))
+  this.addGeneticOperator(new MutationOperator(this, mut))
+  this.addNaturalSelector(new TournamentSelector(this, 3, 0.5), true)
 }
 
+class MaxCubiesFitness(val rubiksCube: RubiksCube) extends FitnessFunction {
 
+  def evaluate(a_subject: IChromosome): Double = {
+
+    var rubiks = rubiksCube.copy
+
+    var fitness = 0.0;
+    var index = 0;
+
+    breakable {
+      for (gene <- a_subject.getGenes) {
+        val newFit = (evaluateCube(rubiks) + sumSides(rubiks)) / (3.0 * 48)
+
+        if (newFit > fitness) {
+          fitness = newFit
+          index = a_subject.getGenes.indexOf(gene)
+        }
+
+        if (rubiks.isSolved) break
+        rubiks.rotateSide(gene.getAllele.asInstanceOf[Int])
+
+      }
+    }
+
+    a_subject.setApplicationData(index)
+    fitness
+
+  }
+
+  def evaluateCube(cube: RubiksCube): Double = {
+    var sum = 0.0
+
+    var tmpSum = 0.0
+    for (i <- 0 to 11) {
+      if (cube.getEdgeCubie(i).isCorrect(rubiksCube)) tmpSum += 1
+    }
+
+    sum += 4 * tmpSum;
+
+    tmpSum = 0.0;
+
+    for (i <- 0 to 7) {
+      if (cube.getCornerCubie(i).isCorrect(rubiksCube)) tmpSum += 1
+    }
+
+    sum + 6 * tmpSum
+  }
+
+  def sumSides(cube: RubiksCube): Double = {
+    var sum = 0.0;
+
+    for (side <- cube.sides) {
+      val center = side.elem(1)(1)
+      for (row <- 0 to 2) {
+        for (col <- 0 to 2)
+          if (side.face(row)(col) == center && (row != 1 || col != 1)) sum += 1
+      }
+
+    }
+
+    sum
+  }
+}
 
 class SideCompleteFitness(val rubiksCube: RubiksCube) extends FitnessFunction {
 
   def evaluate(a_subject: IChromosome): Double = {
 
     var rubiks = rubiksCube.copy
-    
+
     var fitness = 0.0;
     var index = 0;
 
     breakable {
       for (gene <- a_subject.getGenes) {
-         val newFit = sumSides(rubiks)
+        val newFit = sumSides(rubiks) / 48
 
         if (newFit > fitness) {
           fitness = newFit
           index = a_subject.getGenes.indexOf(gene)
         }
-         if (rubiks.isSolved) break;
+        if (rubiks.isSolved) break;
         rubiks.rotateSide(gene.getAllele.asInstanceOf[Int])
       }
     }
@@ -114,7 +189,7 @@ class SideCompleteFitness(val rubiksCube: RubiksCube) extends FitnessFunction {
     fitness
   }
 
-    def sumSides(cube: RubiksCube): Double = {
+  def sumSides(cube: RubiksCube): Double = {
     var sum = 0.0;
 
     for (side <- cube.sides) {
